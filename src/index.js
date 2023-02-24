19f0d5602d85c502896b638c8fb1c7bb3c74bbaf
Lina Budkova <linabudkova35@gmail.com>
2023-02-24 21:21:13 +0200

    af

--- src/index.js
+++ src/index.js
@@ -14,7 +14,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
     <ThemeProvider theme={theme}>
       <PersistGate loading={null} persistor={persistor}>
         <Provider store={store}>
-          <BrowserRouter>
+          <BrowserRouter basename="/goit-react-hw-08-phonebook">
             <App />
           </BrowserRouter>
         </Provider>
